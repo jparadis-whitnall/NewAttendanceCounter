16b9c51fcd08b2c94712a7fff7b57402b41b303f
Jeff Paradis <jparadis@whitnall.com>
2026-05-06 09:01:44 -0500

    Update finalResultsCSV to use target instead of studentTotals

--- index.js
+++ index.js
@@ -256,7 +256,7 @@ document.getElementById("acceptableUsesCalculateResults").addEventListener("clic
         }
 
         if (fate !== null) {
-            finalResultsCSV.push(`${studentId},${lastName},${firstName},${studentTotals},${fate}`);
+            finalResultsCSV.push(`${studentId},${lastName},${firstName},${target},${fate}`);
         }
     }
 
